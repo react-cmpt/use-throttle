51a9d77e336d80893e03909cf30ee06584be98c0
wangcch <ci713@qq.com>
2020-04-10 15:44:29 +0800

    test: useThrottle / useThrottleFn

---
+++ tests/useThrottle.test.ts
@@ -0,0 +1,57 @@
+import {
+  renderHook,
+  act,
+  RenderHookResult,
+} from "@testing-library/react-hooks";
+
+import useThrottle from "../src/useThrottle";
+
+describe("useThrottle", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("should be defined", () => {
+    expect(useThrottle).toBeDefined();
+  });
+
+  it("Init", () => {
+    let hook: RenderHookResult<number, number>;
+
+    act(() => {
+      hook = renderHook(props => useThrottle(props, 200), {
+        initialProps: 0,
+      });
+    });
+
+    act(() => {
+      hook.rerender();
+      expect(hook.result.current).toEqual(0);
+
+      hook.rerender();
+      hook.rerender();
+      hook.rerender();
+      jest.advanceTimersByTime(100);
+      expect(hook.result.current).toEqual(0);
+
+      hook.rerender(1);
+      hook.rerender(2);
+      hook.rerender(3);
+      jest.advanceTimersByTime(200);
+      expect(hook.result.current).toEqual(3);
+
+      hook.rerender(4);
+      hook.rerender(5);
+      jest.advanceTimersByTime(200);
+      expect(hook.result.current).toEqual(5);
+    });
+  });
+});
---
+++ tests/useThrottleFn.test.ts
@@ -0,0 +1,84 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import useThrottleFn from "../src/useThrottleFn";
+
+describe("useThrottleFn", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("should be defined", () => {
+    expect(useThrottleFn).toBeDefined();
+  });
+
+  it("callback and cancel done", () => {
+    let count = 0;
+    const fn = jest.fn(() => count++);
+    const { result, rerender } = renderHook(() => useThrottleFn(fn, 200));
+
+    rerender();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    rerender();
+    rerender();
+    rerender();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    result.current.callback();
+    result.current.callback();
+    result.current.callback();
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(0);
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    result.current.callback();
+    result.current.callback();
+    result.current.callback();
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(1);
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    result.current.callback();
+    result.current.callback();
+    result.current.callback();
+    jest.advanceTimersByTime(200);
+    expect(count).toEqual(2);
+    expect(fn).toHaveBeenCalledTimes(2);
+
+    result.current.callback();
+    result.current.callback();
+    result.current.callback();
+    result.current.cancel();
+    jest.runAllTimers();
+    expect(count).toBe(2);
+  });
+
+  it("callback props", () => {
+    let count = 0;
+    const fn = jest.fn(props => {
+      count = props;
+    });
+    const { result } = renderHook(() => useThrottleFn(fn, 200));
+
+    result.current.callback(1);
+    result.current.callback(2);
+    result.current.callback(3);
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(0);
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    result.current.callback(4);
+    result.current.callback(5);
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(5);
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
