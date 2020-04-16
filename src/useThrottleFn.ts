c56994cd5bb1d2962b1c6e909d95b0aa1cea32e0
wangcch <ci713@qq.com>
2020-04-16 15:54:42 +0800

    feat(useThrottleFn): options (leading)

--- src/useThrottleFn.ts
+++ src/useThrottleFn.ts
@@ -1,19 +1,29 @@
 import { useCallback, useRef, useEffect } from "react";
 
+export type throttleOptions = {
+  /** Specify invoking on the leading edge of the timeout. */
+  leading?: boolean;
+};
+
 /**
  * useThrottleFn
  *
  * @param fn function
  * @param wait number @default 0
+ * @param options object
  */
 export default function useThrottleFn<T extends any[]>(
   fn: (...args: T) => any,
-  wait = 0
+  wait = 0,
+  options?: throttleOptions
 ): { callback: (...args: T) => void; cancel: () => void } {
   const timer = useRef<ReturnType<typeof setTimeout>>();
   const fnRef = useRef(fn);
-  fnRef.current = fn;
+  const optionsRef = useRef<throttleOptions | undefined>(options);
   const currentArgs = useRef<any>();
+
+  fnRef.current = fn;
+  optionsRef.current = options;
 
   const cancel = useCallback(() => {
     if (timer.current) {
@@ -25,11 +35,20 @@ export default function useThrottleFn<T extends any[]>(
   const callback = useCallback(
     (...args: T) => {
       currentArgs.current = args;
+
       if (!timer.current) {
-        timer.current = setTimeout(() => {
+        if (optionsRef.current?.leading) {
           fnRef.current(...currentArgs.current);
-          timer.current = undefined;
-        }, wait);
+
+          timer.current = setTimeout(() => {
+            timer.current = undefined;
+          }, wait);
+        } else {
+          timer.current = setTimeout(() => {
+            fnRef.current(...currentArgs.current);
+            timer.current = undefined;
+          }, wait);
+        }
       }
     },
     [wait]
