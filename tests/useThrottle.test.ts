e2bc46747bd3e2ba0e63033201697098aef6cdcf
wangcch <ci713@qq.com>
2020-04-16 16:05:47 +0800

    test: useThrottle/useThrottleFn (options)

--- tests/useThrottle.test.ts
+++ tests/useThrottle.test.ts
@@ -54,4 +54,35 @@ describe("useThrottle", () => {
       expect(hook.result.current).toEqual(5);
     });
   });
+
+  it("Options: leading", () => {
+    let hook: RenderHookResult<number, number>;
+
+    act(() => {
+      hook = renderHook(props => useThrottle(props, 200, { leading: true }), {
+        initialProps: 0,
+      });
+    });
+
+    act(() => {
+      hook.rerender();
+      expect(hook.result.current).toEqual(0);
+
+      hook.rerender(1);
+      jest.advanceTimersByTime(100);
+      expect(hook.result.current).toEqual(0);
+
+      hook.rerender(2);
+      jest.advanceTimersByTime(1);
+      hook.rerender(3);
+      jest.advanceTimersByTime(99);
+      expect(hook.result.current).toEqual(0);
+
+      hook.rerender(4);
+      jest.advanceTimersByTime(100);
+      hook.rerender(5);
+      jest.advanceTimersByTime(100);
+      expect(hook.result.current).toEqual(4);
+    });
+  });
 });
--- tests/useThrottleFn.test.ts
+++ tests/useThrottleFn.test.ts
@@ -81,4 +81,33 @@ describe("useThrottleFn", () => {
     expect(count).toEqual(5);
     expect(fn).toHaveBeenCalledTimes(1);
   });
+
+  it("Options: leading", () => {
+    let count = 0;
+    const fn = jest.fn(props => {
+      count = props;
+    });
+    const { result } = renderHook(() =>
+      useThrottleFn(fn, 200, { leading: true })
+    );
+
+    result.current.callback(1);
+    result.current.callback(2);
+    result.current.callback(3);
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(1);
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    result.current.callback(4);
+    result.current.callback(5);
+    jest.advanceTimersByTime(100);
+    expect(count).toEqual(1);
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    result.current.callback(6);
+    result.current.callback(7);
+    jest.advanceTimersByTime(200);
+    expect(count).toEqual(6);
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
 });
