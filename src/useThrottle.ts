ac103d0ff4c7c051d9a6130d328b287c3959ecb5
wangcch <ci713@qq.com>
2020-04-16 15:56:15 +0800

    feat(useThrottle): options (leading) and ref callback

--- src/useThrottle.ts
+++ src/useThrottle.ts
@@ -1,22 +1,34 @@
-import { useState, useEffect } from "react";
-import useThrottleFn from "./useThrottleFn";
+import { useState, useEffect, useRef } from "react";
+import useThrottleFn, { throttleOptions } from "./useThrottleFn";
 
 /**
  * useThrottle
  *
  * @param value
  * @param wait number @default 200
+ * @param options object
  */
-export default function useThrottle<T>(value: T, wait = 0): T {
+export default function useThrottle<T>(
+  value: T,
+  wait = 0,
+  options?: throttleOptions
+): T {
   const [state, setState] = useState<T>(value);
 
-  const { callback } = useThrottleFn((v: T) => {
-    setState(v);
-  }, wait);
+  const { callback } = useThrottleFn(
+    (v: T) => {
+      setState(v);
+    },
+    wait,
+    options
+  );
+
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
 
   useEffect(() => {
-    callback(value);
-  }, [callback, value]);
+    callbackRef.current(value);
+  }, [value]);
 
   return state;
 }
