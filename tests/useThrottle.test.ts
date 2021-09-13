1abbfc386f55b22a4f09a8ce551c6d536457769f
wangcch <wangcch.cc@gmail.com>
2021-09-13 14:26:44 +0800

    test(useThrottle): with new faker timer

--- tests/useThrottle.test.ts
+++ tests/useThrottle.test.ts
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import type { RenderHookResult } from "@testing-library/react-hooks";
 import { renderHook, act } from "@testing-library/react-hooks";
 
@@ -5,15 +6,14 @@ import type { ControlFunctions } from "../src/useThrottleFn";
 import useThrottle from "../src/useThrottle";
 
 describe("useThrottle", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     jest.useFakeTimers();
   });
 
   afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.clearAllMocks();
     jest.clearAllTimers();
-  });
-
-  afterAll(() => {
     jest.useRealTimers();
   });
 
@@ -22,35 +22,81 @@ describe("useThrottle", () => {
   });
 
   it("Init", () => {
-    let hook: RenderHookResult<number, [number, ControlFunctions]>;
-
-    void act(() => {
-      hook = renderHook((props: number) => useThrottle(props, 200), {
-        initialProps: 0,
-      });
+    const hook = renderHook((props: number) => useThrottle(props, 200), {
+      initialProps: 0,
     });
 
-    void act(() => {
+    act(() => {
       hook.rerender();
-      expect(hook.result.current[0]).toEqual(0);
+    });
+    expect(hook.result.current[0]).toEqual(0);
 
-      hook.rerender();
-      hook.rerender();
-      hook.rerender();
+    hook.rerender();
+    hook.rerender();
+    hook.rerender();
+    act(() => {
       jest.advanceTimersByTime(100);
-      expect(hook.result.current[0]).toEqual(0);
-
-      hook.rerender(1);
-      hook.rerender(2);
-      hook.rerender(3);
-      jest.advanceTimersByTime(200);
-      expect(hook.result.current[0]).toEqual(3);
-
-      hook.rerender(4);
-      hook.rerender(5);
-      jest.advanceTimersByTime(200);
-      expect(hook.result.current[0]).toEqual(5);
     });
+    expect(hook.result.current[0]).toEqual(0);
+    jest.advanceTimersByTime(100);
+    expect(hook.result.current[0]).toEqual(0);
+
+    hook.rerender(1);
+    hook.rerender(2);
+    hook.rerender(3);
+    act(() => {
+      jest.advanceTimersByTime(200);
+    });
+    expect(hook.result.current[0]).toEqual(3);
+
+    hook.rerender(4);
+    hook.rerender(5);
+    act(() => {
+      jest.advanceTimersByTime(200);
+    });
+    expect(hook.result.current[0]).toEqual(5);
+
+    hook.unmount();
+  });
+
+  it("state value", () => {
+    const { result, unmount } = renderHook(() => {
+      const [num, setNum] = useState(0);
+      const [value] = useThrottle(num, 200);
+
+      const callback = useCallback((n: number) => {
+        setNum(n);
+      }, []);
+
+      return {
+        callback,
+        value,
+      };
+    });
+
+    expect(result.current.value).toEqual(0);
+
+    act(() => {
+      result.current.callback(1);
+      result.current.callback(2);
+      result.current.callback(3);
+      jest.advanceTimersByTime(200);
+    });
+
+    expect(result.current.value).toEqual(3);
+
+    act(() => {
+      result.current.callback(4);
+    });
+    expect(result.current.value).toEqual(3);
+
+    act(() => {
+      result.current.callback(5);
+      jest.advanceTimersByTime(200);
+    });
+    expect(result.current.value).toEqual(5);
+
+    unmount();
   });
 
   it("Options: leading", () => {
@@ -95,70 +141,72 @@ describe("useThrottle", () => {
 
     const customizer = (l: Obj, r: Obj) => l.b != r.b;
 
-    let hook: RenderHookResult<Obj, [Obj, ControlFunctions]>;
+    const hook = renderHook(
+      (props: Obj) => useThrottle(props, 200, { customizer }),
+      {
+        initialProps: obj1,
+      }
+    );
 
-    void act(() => {
-      hook = renderHook(
-        (props: Obj) => useThrottle(props, 200, { customizer }),
-        {
-          initialProps: obj1,
-        }
-      );
-    });
+    hook.rerender();
+    expect(hook.result.current[0]).toEqual(obj1);
 
-    void act(() => {
-      hook.rerender();
-      expect(hook.result.current[0]).toEqual(obj1);
-
-      hook.rerender(obj2);
+    hook.rerender(obj2);
+    act(() => {
       jest.advanceTimersByTime(100);
-      expect(hook.result.current[0]).toEqual(obj1);
+    });
+    expect(hook.result.current[0]).toEqual(obj1);
 
-      hook.rerender(obj3);
+    hook.rerender(obj3);
+    act(() => {
       jest.advanceTimersByTime(1);
-      hook.rerender(obj3);
       jest.advanceTimersByTime(99);
-      expect(hook.result.current[0]).toEqual(obj1);
-
-      hook.rerender(obj3);
-      jest.advanceTimersByTime(100);
-      expect(hook.result.current[0]).toEqual(obj3);
     });
+    expect(hook.result.current[0]).toEqual(obj1);
+
+    hook.rerender(obj3);
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+    expect(hook.result.current[0]).toEqual(obj3);
   });
 
   it("Return functions", () => {
-    const { rerender, result } = renderHook(
+    const { rerender, result, unmount } = renderHook(
       (props: number) => useThrottle(props, 200),
       {
         initialProps: 0,
       }
     );
 
-    void act(() => {
-      rerender();
+    rerender();
+    act(() => {
+      jest.advanceTimersByTime(200);
     });
     expect(result.current[0]).toEqual(0);
 
-    void act(() => {
-      rerender(4);
-      rerender(5);
+    rerender(4);
+    rerender(5);
+    act(() => {
       jest.advanceTimersByTime(200);
     });
     expect(result.current[0]).toEqual(5);
 
-    void act(() => {
-      rerender(6);
+    rerender(6);
+    act(() => {
       jest.advanceTimersByTime(100);
       result.current[1].cancel();
       jest.advanceTimersByTime(100);
     });
     expect(result.current[0]).toEqual(5);
 
-    void act(() => {
-      rerender(7);
+    rerender(7);
+    act(() => {
       jest.advanceTimersByTime(100);
       result.current[1].callPending();
     });
     expect(result.current[0]).toEqual(7);
+
+    unmount();
   });
 });
